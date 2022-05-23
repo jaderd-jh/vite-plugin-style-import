0688ff40bceae6c1bcc2f4d815eebaf7706fa0e6
章雯婷 <wenting.zhang@jaderd.com>
2022-05-23 10:17:32 +0800

    🚀 chore: release 1.0.3

--- packages/core/src/resolve/antdMobile.ts
+++ packages/core/src/resolve/antdMobile.ts
@@ -1,6 +1,6 @@
 import type { Lib } from '../typing'
 
-export function AntdMobile(): Lib {
+export function AntdMobileResolve(): Lib {
   return {
     libraryName: 'antd-mobile',
     esModule: true,
